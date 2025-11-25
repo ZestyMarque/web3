4b8d7396b90fd634304ef65559fcbed5f94cee7f
ZestyMarque <144468826+ZestyMarque@users.noreply.github.com>
2025-11-25 03:18:40 +0300

    Update game.js

--- src/js/game.js
+++ src/js/game.js
@@ -1,114 +1,151 @@
-export class Game2048 {
-    constructor(size = 4) {
-        this.size = size;
-        this.grid = this.createEmptyGrid();
+import { deepCopy } from './utils.js';
+
+export class Game {
+    constructor() {
+        this.size = 4;
+        this.board = Array.from({ length: this.size }, () => Array(this.size).fill(0));
         this.score = 0;
-        this.history = [];
+        this.previousBoard = null;
+        this.previousScore = null;
+        this.isOver = false;
+        const initialTiles = Math.floor(Math.random() * 3) + 1; 
+        this.addRandomTiles(initialTiles);
     }
 
-    createEmptyGrid() {
-        return Array.from({ length: this.size }, () => Array(this.size).fill(0));
+    savePreviousState() {
+        this.previousBoard = deepCopy(this.board);
+        this.previousScore = this.score;
     }
 
-    saveState() {
-        this.history.push({ grid: this.cloneGrid(this.grid), score: this.score });
-        if (this.history.length > 50) this.history.shift();
-    }
-
-    undo() {
-        if (this.history.length === 0) return false;
-        const prev = this.history.pop();
-        this.grid = this.cloneGrid(prev.grid);
-        this.score = prev.score;
+    addRandomTile() {
+        const emptyCells = [];
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.board[i][j] === 0) {
+                    emptyCells.push([i, j]);
+                }
+            }
+        }
+        if (emptyCells.length === 0) return false;
+        const [x, y] = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+        this.board[x][y] = Math.random() < 0.9 ? 2 : 4;
         return true;
     }
 
-    cloneGrid(grid) {
-        return grid.map(row => [...row]);
-    }
-
-    spawnTile() {
-        const empty = [];
-        for (let r = 0; r < this.size; r++) {
-            for (let c = 0; c < this.size; c++) {
-                if (this.grid[r][c] === 0) empty.push({ r, c });
-            }
+    addRandomTiles(count) {
+        for (let i = 0; i < count; i++) {
+            this.addRandomTile();
         }
-        if (empty.length === 0) return;
-
-        const { r, c } = empty[Math.floor(Math.random() * empty.length)];
-        this.grid[r][c] = Math.random() < 0.9 ? 2 : 4;
     }
 
     move(direction) {
-        this.saveState();
-
+        if (this.isOver) return false;
+        this.savePreviousState();
         let moved = false;
-        const combineScore = { value: 0 };
+        let addedScore = 0;
+        let boardCopy = deepCopy(this.board);
 
-        const newGrid = this.cloneGrid(this.grid);
-
-        const moveRow = row => {
-            const filtered = row.filter(v => v !== 0);
-            const merged = [];
-            let i = 0;
-
-            while (i < filtered.length) {
-                if (i + 1 < filtered.length && filtered[i] === filtered[i + 1]) {
-                    const newVal = filtered[i] * 2;
-                    merged.push(newVal);
-                    combineScore.value += newVal;
-                    i += 2;
-                } else {
-                    merged.push(filtered[i]);
-                    i++;
-                }
-            }
-
-            while (merged.length < this.size) merged.push(0);
-            return merged;
-        };
-
-        const rotateRight = grid => {
-            const res = this.createEmptyGrid();
-            for (let r = 0; r < this.size; r++) {
-                for (let c = 0; c < this.size; c++) {
-                    res[c][this.size - 1 - r] = grid[r][c];
-                }
-            }
-            return res;
-        };
-
-        let temp = newGrid;
-        for (let i = 0; i < direction; i++) temp = rotateRight(temp);
-
-        for (let r = 0; r < this.size; r++) {
-            const newRow = moveRow(temp[r]);
-            if (newRow.some((v, idx) => v !== temp[r][idx])) moved = true;
-            temp[r] = newRow;
+        if (direction === 'left') {
+            [moved, addedScore] = this.slideLeft(boardCopy);
+        } else if (direction === 'right') {
+            boardCopy = this.reverseRows(boardCopy);
+            [moved, addedScore] = this.slideLeft(boardCopy);
+            boardCopy = this.reverseRows(boardCopy);
+        } else if (direction === 'up') {
+            boardCopy = this.transpose(boardCopy);
+            [moved, addedScore] = this.slideLeft(boardCopy);
+            boardCopy = this.transpose(boardCopy);
+        } else if (direction === 'down') {
+            boardCopy = this.transpose(boardCopy);
+            boardCopy = this.reverseRows(boardCopy);
+            [moved, addedScore] = this.slideLeft(boardCopy);
+            boardCopy = this.reverseRows(boardCopy);
+            boardCopy = this.transpose(boardCopy);
         }
-
-        for (let i = 0; i < (4 - direction) % 4; i++) temp = rotateRight(temp);
 
         if (moved) {
-            this.grid = temp;
-            this.score += combineScore.value;
-            this.spawnTile();
+            this.board = boardCopy;
+            this.score += addedScore;
+            const newTiles = Math.floor(Math.random() * 2) + 1;
+            this.addRandomTiles(newTiles);
+            this.checkGameOver();
+            return true;
         } else {
-            this.history.pop();
+            this.previousBoard = null;
+            this.previousScore = null;
+            return false;
         }
-
-        return moved;
     }
 
-    isGameOver() {
-        for (let r = 0; r < this.size; r++) {
-            for (let c = 0; c < this.size; c++) {
-                if (this.grid[r][c] === 0) return false;
-                if (c < this.size - 1 && this.grid[r][c] === this.grid[r][c + 1]) return false;
-                if (r < this.size - 1 && this.grid[r][c] === this.grid[r + 1][c]) return false;
+    slideLeft(board) {
+        let moved = false;
+        let addedScore = 0;
+        for (let i = 0; i < this.size; i++) {
+            let row = board[i].filter(cell => cell !== 0);
+            let j = 0;
+            while (j < row.length - 1) {
+                if (row[j] === row[j + 1]) {
+                    row[j] *= 2;
+                    addedScore += row[j];
+                    row.splice(j + 1, 1);
+                } else {
+                    j++;
+                }
+            }
+            while (row.length < this.size) row.push(0);
+            if (!board[i].every((val, idx) => val === row[idx])) moved = true;
+            board[i] = row;
+        }
+        return [moved, addedScore];
+    }
+
+    reverseRows(board) {
+        return board.map(row => row.slice().reverse());
+    }
+
+    transpose(board) {
+        return board[0].map((_, colIndex) => board.map(row => row[colIndex]));
+    }
+
+    checkGameOver() {
+        if (this.hasEmptyCells() || this.hasAdjacentMatches()) {
+            this.isOver = false;
+        } else {
+            this.isOver = true;
+        }
+    }
+
+    hasEmptyCells() {
+        return this.board.flat().some(cell => cell === 0);
+    }
+
+    hasAdjacentMatches() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (j < this.size - 1 && this.board[i][j] === this.board[i][j + 1]) return true;
+                if (i < this.size - 1 && this.board[i][j] === this.board[i + 1][j]) return true;
             }
         }
-        return true;
+        return false;
+    }
+
+    undo() {
+        if (this.previousBoard && !this.isOver) {
+            this.board = this.previousBoard;
+            this.score = this.previousScore;
+            this.previousBoard = null;
+            this.previousScore = null;
+            this.isOver = false;
+        }
+    }
+
+    reset() {
+        this.board = Array.from({ length: this.size }, () => Array(this.size).fill(0));
+        this.score = 0;
+        this.previousBoard = null;
+        this.previousScore = null;
+        this.isOver = false;
+        const initialTiles = Math.floor(Math.random() * 3) + 1;
+        this.addRandomTiles(initialTiles);
     }
 }
