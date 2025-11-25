237483f756e9cf1c158ba5072271075c7a266d85
ZestyMarque <144468826+ZestyMarque@users.noreply.github.com>
2025-11-25 16:42:47 +0300

    Update storage.js

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,1 +1,12 @@
+function saveGameState(board, score) {
+    localStorage.setItem('gameState', JSON.stringify({ board, score }));
+}
 
+function loadGameState() {
+    const savedState = localStorage.getItem('gameState');
+    return savedState ? JSON.parse(savedState) : null;
+}
+
+function clearGameState() {
+    localStorage.removeItem('gameState');
+}
